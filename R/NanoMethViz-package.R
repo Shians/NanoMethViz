caadaa20e29734490b3059cf38184889dfda9462
Shians <registertonysu@gmail.com>
2020-04-15 21:17:06 +1000

    Added package documentation

---
+++ R/NanoMethViz-package.R
@@ -0,0 +1,11 @@
+#' @details The main plotting functions in this package are [plot_gene()] and
+#'   [plot_region()].
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL

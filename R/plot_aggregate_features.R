e3c1d8c08731d43ac9d6d65a4be125230191128b
Shians <registertonysu@gmail.com>
2020-04-23 16:59:42 +1000

    Added prototype for plot_aggregate_features

--- R/plot_aggregate_features.R
+++ R/plot_aggregate_features.R
@@ -1,24 +1,9 @@
+#' @export
 plot_aggregate_features <- function(x, features, group_by = NULL) {
-    assert_that(is.string(group_by))
+    assert_that(is.null(group_by) || is.string(group_by))
     assert_that(features %has_name% group_by)
 
-    .split_rows <- function(x) {
-        split(x, 1:nrow(x))
-    }
-
-    .row_map <- function(.x, .f, ...) {
-        purrr::map(.split_rows(.x), .f, ...)
-    }
-
-    .get_feature <- function(feature, methy) {
-        .scale_to_feature <- function(x) {
-            (x - feature$start) / (feature$end - feature$start)
-        }
-        query_methy(methy, feature$chr, feature$start, feature$end) %>%
-            dplyr::mutate(rel_pos = .scale_to_feature(.data$pos))
-    }
-
-    methy_data <- .row_map(features, .get_feature, methy = methy(nmr))
+    methy_data <- .get_features(features, methy = methy(nmr))
 
     if (!is.null(group_by)) {
         names(methy_data) <- features[[group_by]]
@@ -27,11 +12,13 @@ plot_aggregate_features <- function(x, features, group_by = NULL) {
     methy_data <- methy_data %>%
         dplyr::bind_rows(.id = "group")
 
-    p <-
-        ggplot(aes(
-            x = rel_pos,
-            y = e1071::sigmoid(statistic)
-        ), data = methy_data) +
+    p <- ggplot(
+            aes(
+                x = rel_pos,
+                y = e1071::sigmoid(statistic)
+            ),
+            data = methy_data
+        ) +
         ylim(c(0, 1)) +
         ggthemes::theme_tufte()
 
@@ -40,4 +27,59 @@ plot_aggregate_features <- function(x, features, group_by = NULL) {
     } else {
         p + geom_smooth(na.rm = TRUE)
     }
+}
+
+#' @export
+plot_aggregate_features2 <- function(x, features) {
+    methy_data <- .get_features(features, methy = methy(nmr))
+
+    methy_data <- methy_data %>%
+        dplyr::bind_rows() %>%
+        dplyr::inner_join(samples(x), by = "sample")
+
+    p <- ggplot(
+            aes(
+                x = rel_pos,
+                y = e1071::sigmoid(statistic)
+            ),
+            data = methy_data) +
+        ylim(c(0, 1)) +
+        ggthemes::theme_tufte()
+
+    if (!is.null(group_by)) {
+        p + geom_smooth(aes(group = group, colour = group), na.rm = TRUE)
+    } else {
+        p + geom_smooth(na.rm = TRUE)
+    }
+}
+
+.split_rows <- function(x) {
+    split(x, 1:nrow(x))
+}
+
+.row_map <- function(.x, .f, ...) {
+    parallel::mclapply(.split_rows(.x), .f, mc.cores = 4L, ...)
+}
+
+.get_features <- function(features, methy, feature_ids = NULL) {
+    .scale_to_feature <- function(x, feature) {
+        (x - feature$start) / (feature$end - feature$start)
+    }
+
+    out <- query_methy(methy, features$chr, features$start, features$end)
+    out <- purrr::map2(out, .split_rows(features), function(x, y) {
+        if (nrow(x) == 0) {
+            return(tibble::add_column(x, rel_pos = numeric()))
+        }
+
+        x$rel_pos <- .scale_to_feature(x$pos, y)
+        x
+    })
+
+    if (!is.null(feature_ids)) {
+        assert_that(length(feature_ids) == nrow(out))
+        names(out) <- feature_ids
+    }
+
+    out
 }

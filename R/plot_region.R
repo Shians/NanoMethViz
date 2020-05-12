97023ecf7fd076d49e7a0726845bcea087cd5853
shians <registertonysu@gmail.com>
2020-05-12 11:09:56 +1000

    Reformatted code

--- R/plot_region.R
+++ R/plot_region.R
@@ -9,7 +9,7 @@
 #' @return None
 #' @export
 setGeneric("plot_region", function(x, chr, start, end, ...) {
-  standardGeneric("plot_region")
+    standardGeneric("plot_region")
 })
 
 #' @rdname plot_region
@@ -18,20 +18,62 @@ setGeneric("plot_region", function(x, chr, start, end, ...) {
 #' @param spaghetti whether or not individual reads should be shown.
 #' @param span the span for loess smoothing.
 #'
+#' @expor
+setMethod("plot_region",
+    signature(
+        x = "NanoMethResult",
+        chr = "character",
+        start = "numeric",
+        end = "numeric"),
+    function(
+        x,
+        chr,
+        start,
+        end,
+        anno_regions = NULL,
+        spaghetti = FALSE,
+        span = NULL
+        ) {
+        .plot_region(
+            x = x,
+            chr = chr,
+            start = start,
+            end = end,
+            anno_regions = anno_regions,
+            spaghetti = spaghetti,
+            span = span
+        )
+    })
+
+#' @rdname plot_region
+#'
 #' @export
-setMethod("plot_region", signature(x = "NanoMethResult", chr = "character", start = "numeric", end = "numeric"),
-    function(x, chr, start, end, anno_regions = NULL, spaghetti = FALSE, span = NULL) {
-      .plot_region(
-        x = x,
-        chr = chr,
-        start = start,
-        end = end,
-        anno_regions = anno_regions,
-        spaghetti = spaghetti,
-        span = span
-      )
-    }
-)
+setMethod("plot_region",
+    signature(
+        x = "NanoMethResult",
+        chr = "factor",
+        start = "numeric",
+        end = "numeric"),
+    function(
+        x,
+        chr,
+        start,
+        end,
+        anno_regions = NULL,
+        spaghetti = FALSE,
+        span = NULL
+        ) {
+        chr <- as.character(chr)
+        plot_region(
+            x = x,
+            chr = chr,
+            start = start,
+            end = end,
+            anno_regions = anno_regions,
+            spaghetti = spaghetti,
+            span = span
+        )
+    })
 
 
 .plot_region <- function(
@@ -41,24 +83,42 @@ setMethod("plot_region", signature(x = "NanoMethResult", chr = "character", star
     end,
     anno_regions = NULL,
     spaghetti = FALSE,
-    span = NULL
+    span = NULL,
+    window_prop = c(0.3, 0.3)
     ) {
     sample_anno <- samples(x)
-    exons_anno <- query_exons_region(exons(x), chr = chr, start = start, end = end)
+    exons_anno <- query_exons_region(
+        exons(x),
+        chr = chr,
+        start = start,
+        end = end
+    )
 
-    feature <- list()
-    feature$chr <- chr
-    feature$start <- start
-    feature$end <- end
+    window_left <- (end - start) * window_prop[1]
+    window_right <- (end - start) * window_prop[2]
 
-    p1 <- with(exons_anno,
-         plot_feature(
-            feature,
+    methy_data <-
+        query_methy(methy(x), chr, start - window_left, end + window_right) %>%
+        dplyr::bind_rows() %>%
+        dplyr::select(-"strand", -"modified") %>%
+        tibble::as_tibble()
+
+    if (nrow(methy_data) == 0) {
+        warning("no methylation data in region")
+        return(ggplot() + theme_void())
+    }
+
+    p1 <- with(
+        exons_anno,
+        plot_methylation_internal(
+            methy_data = methy_data,
+            start = start,
+            end = end,
+            chr = chr,
             title = glue::glue("{chr}:{start}-{end}"),
-            methy = methy(x),
-            sample_anno = sample_anno,
             anno_regions = anno_regions,
             spaghetti = spaghetti,
+            sample_anno = sample_anno,
             span = span
         )
     )
@@ -67,7 +127,9 @@ setMethod("plot_region", signature(x = "NanoMethResult", chr = "character", star
 
     p2 <- plot_gene_annotation(exons_anno, xlim[1], xlim[2])
 
-    n_unique <- function(x) { length(unique(x)) }
+    n_unique <- function(x) {
+        length(unique(x))
+    }
 
     heights <- c(1, 0.075 * n_unique(exons_anno$transcript_id))
     p1 / p2 + patchwork::plot_layout(heights = heights)

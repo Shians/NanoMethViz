12cdd6be197e8a6955d364bcd62c4b38995fd71a
Shians <registertonysu@gmail.com>
2021-03-10 11:31:44 +1100

    Added tests

--- tests/testthat/test-plot_agg_regions.R
+++ tests/testthat/test-plot_agg_regions.R
@@ -6,3 +6,20 @@ test_that("Aggregate plotting works", {
     expect_silent(plot_agg_regions(nmr, NanoMethViz::exons(nmr)))
     expect_silent(plot_agg_regions_sample_grouped(nmr, NanoMethViz::exons(nmr)))
 })
+
+test_that("Aggregate plotting error checking works", {
+    # setup
+    nmr <- load_example_nanomethresult()
+    missing_chr <- NanoMethViz::exons(nmr)[, -2]
+    missing_chr_list <- list(
+        NanoMethViz::exons(nmr)[, -2],
+        NanoMethViz::exons(nmr),
+        NanoMethViz::exons(nmr)[, -2]
+    )
+
+    # test
+    expect_error(plot_agg_regions(nmr, missing_chr))
+    expect_error(plot_agg_regions(nmr, missing_chr_list))
+    expect_error(plot_agg_regions_sample_grouped(nmr, missing_chr))
+    expect_error(plot_agg_regions_sample_grouped(nmr, missing_chr_list))
+})

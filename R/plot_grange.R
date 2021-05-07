34e6e463c35dbfadd5fe4fcf823d8efee0f95f8b
Shians <registertonysu@gmail.com>
2021-05-07 15:32:13 +1000

    Added granges version of heatmap and added window_prop argument to grange variants of plotting functions

--- R/plot_grange.R
+++ R/plot_grange.R
@@ -15,7 +15,8 @@ plot_grange <- function(
     grange,
     anno_regions = NULL,
     spaghetti = FALSE,
-    span = NULL
+    span = NULL,
+    window_prop = 0.3
 ) {
 
     assert_that(
@@ -34,6 +35,51 @@ plot_grange <- function(
         end = end,
         anno_regions = anno_regions,
         spaghetti = spaghetti,
-        span = span
+        span = span,
+        window_prop = window_prop
+    )
+}
+
+#' @rdname plot_gene_heatmap
+#'
+#' @param window_prop the size of flanking region to plot. Can be a vector of two
+#'   values for left and right window size. Values indicate proportion of gene
+#'   length.
+#' @param pos_style the style for plotting the base positions along the x-axis.
+#'   Defaults to "to_scale", plotting (potentially) overlapping squares
+#'   along the genomic position to scale. The "compact" options plots only the
+#'   positions with measured modification.
+#'
+#' @return a ggplot plot containing the heatmap.
+#'
+#' @examples
+#' nmr <- load_example_nanomethresult()
+#' gr <- GenomicRanges::GRanges(data.frame(chr = "chr7", start = 6703892, end = 6730431))
+#' plot_grange_heatmap(nmr, gr[1, ])
+#'
+#' @export
+plot_grange_heatmap <- function(
+    x,
+    grange,
+    pos_style = c("to_scale", "compact"),
+    window_prop = 0.3
+) {
+
+    assert_that(
+        is(grange, "GRanges"),
+        length(grange) == 1
+    )
+
+    chr <- as.character(GenomicRanges::seqnames(grange))
+    start <- GenomicRanges::start(grange)
+    end <- GenomicRanges::end(grange)
+
+    plot_region_heatmap(
+        x,
+        chr = chr,
+        start = start,
+        end = end,
+        pos_style = pos_style,
+        window_prop = window_prop
     )
 }

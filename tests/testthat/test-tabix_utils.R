7ccac35b476fe1b5b2751ac2dc4b78da2834610d
Shians <registertonysu@gmail.com>
2021-08-10 10:09:14 +1000

    Added test for importing megalodon

--- tests/testthat/test-tabix_utils.R
+++ tests/testthat/test-tabix_utils.R
@@ -1,4 +1,4 @@
-test_that("Importing works", {
+test_that("Importing nanopolish works", {
     # setup
     methy_calls <- system.file(package = "NanoMethViz",
         c("sample1_nanopolish.tsv.gz", "sample2_nanopolish.tsv.gz"))
@@ -8,4 +8,15 @@ test_that("Importing works", {
     # test
     expect_message(create_tabix_file(methy_calls, temp_file))
     expect_true(is(methy_to_bsseq(temp_file), "BSseq"))
+})
+
+test_that("Importing megalodon works", {
+    # setup
+    methy_calls <- system.file(package = "NanoMethViz",
+       "megalodon_calls.txt.gz")
+    temp_file <- paste0(tempfile(), ".tsv.bgz")
+    withr::defer(file.remove(temp_file))
+
+    # test
+    expect_message(create_tabix_file(methy_calls, temp_file))
 })

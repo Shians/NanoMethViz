e64b03869fc176ae5fdd09631b623cec971b648a
Shians <registertonysu@gmail.com>
2022-06-01 12:28:02 +1000

    Typo fix

--- R/plot_grange.R
+++ R/plot_grange.R
@@ -20,7 +20,7 @@ plot_grange <- function(
     heatmap = FALSE,
     span = NULL,
     window_prop = 0,
-    palette = ggplot2::scale_colour_brewer(palette = "Set1"),.
+    palette = ggplot2::scale_colour_brewer(palette = "Set1"),
     line_size = 2
 ) {
     avg_method <- match.arg(avg_method)

e36c176a2a148b7a85e4887b4522406ed1fdb959
Shians <registertonysu@gmail.com>
2022-06-07 11:15:21 +1000

    Added `merge_methy_files()` prototype

---
+++ R/merge_methy_files.R
@@ -0,0 +1,31 @@
+merge_methy_files <- function(inputs, output) {
+    assertthat::assert_that(
+        all(fs::is_file(inputs)),
+        all(fs::file_exists(inputs))
+    )
+
+    assertthat::assert_that(
+        string::str_detect(output, "[.]bgz$"),
+        msg = "'output' must end in .bgz"
+    )
+
+    output <- stringr::str_remove(output, ".bgz")
+
+    temp_files <- purrr::map_chr(seq_along(inputs))
+
+    temp_merged <- tempfile()
+
+    purrr::walk2(
+        inputs,
+        temp_files,
+        ~R.utils::gunzip(.x, destname = .y, remove = FALSE)
+    )
+
+    files_str <- paste(temp_files, collapse = " ")
+    cmd <- glue::glue("sort -k2,3V -o {temp_merged} {files_str}")
+    system(cmd)
+
+    fs::file_copy(temp_merged, output)
+    NanoMethViz:::tabix_compress(output)
+}
+

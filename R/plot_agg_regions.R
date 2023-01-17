Shians/NanoMethViz

commit fd4f191faf19e8d0f16678c6784eab7a26c5eba4

Fixed deprecation compliance with tidyselect 1.2.0, ggplot 3.4.0

R/plot_agg_regions.R
@@ -59,9 +59,9 @@ plot_agg_regions <- function(
         ggplot2::ylim(c(0, 1)) +
         ggplot2::theme_bw() +
         ggplot2::stat_smooth(
-            ggplot2::aes_string(
-                x = "binned_pos",
-                y = "methy_prop",
+            ggplot2::aes(
+                x = .data$binned_pos,
+                y = .data$methy_prop,
                 group = group_col,
                 col = group_col),
             method = "loess",

R/plot_mds.R
@@ -62,9 +62,9 @@ plot_mds <- function(x, top = 500, plot_dims = c(1, 2), labels = colnames(x), gr
 
 
     if (!is.null(groups)) {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes_string(x = "dim1", y = "dim2", col = "groups"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = "groups"))
     } else {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes_string(x = "dim1", y = "dim2"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2))
     }
 
     if (!is.null(labels)) {

R/plot_pca.R
@@ -43,13 +43,13 @@ plot_pca <- function(x, plot_dims = c(1, 2), labels = colnames(x), groups = NULL
     ylabel <- glue::glue("PCA Dim {plot_dims[2]}")
 
     if (!is.null(groups)) {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes_string(x = "dim1", y = "dim2", col = "groups"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = "groups"))
     } else {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes_string(x = "dim1", y = "dim2"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2))
     }
 
     if (!is.null(labels)) {
-        p <- p + ggplot2::geom_label(ggplot2::aes_string(label = "labels"))
+        p <- p + ggplot2::geom_label(ggplot2::aes(label = .data$labels))
     } else {
         p <- p + ggplot2::geom_point()
 

DESCRIPTION
@@ -17,7 +17,7 @@ BugReports: https://github.com/Shians/NanoMethViz/issues
 Depends:
     R (>= 4.0.0),
     methods,
-    ggplot2
+    ggplot2 (>= 3.4.0)
 Imports:
     cpp11 (>= 0.2.5),
     readr,

R/convert_methy_format.R
@@ -62,9 +62,9 @@ reformat_nanopolish <- function(x, sample) {
 reformat_megalodon_old <- function(x, sample) {
     x %>%
         rename(
-            chr = .data$chrm,
-            statistic = .data$mod_log_prob,
-            read_name = .data$read_id) %>%
+            chr = "chrm",
+            statistic = "mod_log_prob",
+            read_name = "read_id") %>%
         add_column(sample = sample, .before = 1) %>%
         mutate(
             sample = as.factor(.data$sample),
@@ -82,9 +82,9 @@ reformat_megalodon_old <- function(x, sample) {
 reformat_megalodon <- function(x, sample) {
     x %>%
         rename(
-            chr = .data$chrm,
-            statistic = .data$mod_log_prob,
-            read_name = .data$read_id) %>%
+            chr = "chrm",
+            statistic = "mod_log_prob",
+            read_name = "read_id") %>%
         add_column(sample = sample, .before = 1) %>%
         mutate(
             sample = as.factor(.data$sample),

R/plot_gene_annotation.R
@@ -29,7 +29,7 @@ plot_gene_annotation <- function(exons_df, plot_start, plot_end) {
                 gap_start = list(.data$end[-length(.data$end)]),
                 gap_end = list(.data$start[-1])
             ) %>%
-            tidyr::unnest(cols = c(.data$gap_start, .data$gap_end))
+            tidyr::unnest(cols = c("gap_start", "gap_end"))
     } else {
         gap <- tibble::tibble(
             uid = character(),

R/plot_gene_heatmap.R
@@ -147,7 +147,7 @@ setMethod(
         dplyr::nest_by(.data$group, .data$read_group) %>%
         dplyr::group_by(.data$group) %>%
         dplyr::group_modify(subsample_groups, subsample = subsample)
-    methy_data <- tidyr::unnest(methy_data, .data$data)
+    methy_data <- tidyr::unnest(methy_data, "data")
     read_data <- read_data %>%
         dplyr::filter(.data$read_name %in% methy_data$read_name)
 

R/plot_methylation.R
@@ -94,7 +94,7 @@ plot_methylation_internal <- function(
             data = plot_data_smooth,
             span = span,
             na.rm = TRUE,
-            size = line_size
+            linewidth = line_size
         )
 
     # add auxiliary elements and style

commit 8c2c1244347e494d04d118fbd9a6abe4a9872d09

Fixed tidy semantics broken in fd4f191

R/plot_agg_regions.R
@@ -62,8 +62,8 @@ plot_agg_regions <- function(
             ggplot2::aes(
                 x = .data$binned_pos,
                 y = .data$methy_prop,
-                group = group_col,
-                col = group_col),
+                group = .data[[group_col]],
+                col = .data[[group_col]]),
             method = "loess",
             formula = "y ~ x",
             span = span,

R/plot_mds.R
@@ -62,7 +62,7 @@ plot_mds <- function(x, top = 500, plot_dims = c(1, 2), labels = colnames(x), gr
 
 
     if (!is.null(groups)) {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = "groups"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = .data$groups))
     } else {
         p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2))
     }

R/plot_pca.R
@@ -43,7 +43,7 @@ plot_pca <- function(x, plot_dims = c(1, 2), labels = colnames(x), groups = NULL
     ylabel <- glue::glue("PCA Dim {plot_dims[2]}")
 
     if (!is.null(groups)) {
-        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = "groups"))
+        p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2, col = .data$groups))
     } else {
         p <- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data$dim1, y = .data$dim2))
     }
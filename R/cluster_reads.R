078dc5790eb5803d84ba7cf6cc140597470929ca
Shians <registertonysu@gmail.com>
2023-05-02 11:24:54 +1000

    Added cluster_read() function

---
+++ R/cluster_reads.R
@@ -0,0 +1,57 @@
+cluster_reads <- function(x, chr, start, end) {
+    methy_data <- query_methy(x, chr, start, end ) %>%
+        filter(pos >= start & pos < end)
+
+    read_stats <- methy_data %>%
+        group_by(read_name) %>%
+        summarise(
+            start = min(pos),
+            end = max(pos),
+            span = end - start,
+            strand = unique(strand)
+        ) %>%
+        arrange(strand)
+
+    max_span <- max(read_stats$span)
+
+    keep_reads <- read_stats$read_name[read_stats$span > 0.9 * max_span]
+
+    methy_data <- methy_data %>%
+        dplyr::filter(read_name %in% keep_reads)
+
+    mod_mat <- methy_data %>%
+        select(read_name, pos, mod_prob) %>%
+        arrange(pos) %>%
+        pivot_wider(names_from = pos, values_from = mod_prob) %>%
+        df_to_matrix()
+
+    mod_mat_filled <- mod_mat[order(rownames(mod_mat)), ]
+
+    col_missingness <- mat_col_map(mod_mat_filled, missingness)
+    mod_mat_filled <- mod_mat_filled[, col_missingness < 0.8]
+
+    row_missingness <- mat_row_map(mod_mat_filled, missingness)
+    mod_mat_filled <- mod_mat_filled[row_missingness < 0.2, ]
+    for (i in 1:nrow(mod_mat_filled)) {
+        mod_mat_filled[i, is.na(mod_mat_filled[i, ])] <- mean(mod_mat_filled[i, ], na.rm = TRUE)
+    }
+
+    dbsc <- hdbscan(temp_filled, minPts = 15)
+    clust_df <- data.frame(read_name = rownames(temp_filled), cluster_id = dbsc$cluster)
+
+    out_df <- as.data.frame(mod_mat) %>%
+        tibble::rownames_to_column("read_name") %>%
+        tidyr::pivot_longer(
+            cols = !dplyr::contains("read_name"),
+            names_to = "pos",
+            values_to = "methy_prob"
+        )
+
+    out_df %>%
+        group_by(read_name) %>%
+        summarise(mean = mean(methy_prob, na.rm = TRUE)) %>%
+        left_join(clust_df, by = "read_name") %>%
+        left_join(read_stats, by = "read_name") %>%
+        arrange(cluster_id) %>%
+        print(n = Inf)
+}

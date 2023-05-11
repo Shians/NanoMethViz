9b0468f204910aa33c4ba25399894eb0cef7ac86
Shians <registertonysu@gmail.com>
2023-05-11 16:09:31 +1000

    Refactored heatmap plot

--- R/plot_gene_heatmap.R
+++ R/plot_gene_heatmap.R
@@ -88,120 +88,18 @@ setMethod(
 
     pos_range <- gene_pos_range(x, gene)
 
-    gene_width <- pos_range[2] - pos_range[1]
-    window_left <- gene_width * window_prop[1]
-    window_right <- gene_width * window_prop[2]
-
-    plot_left <- pos_range[1] - window_left
-    plot_right <- pos_range[2] + window_right
-
     chr <- exons(x) %>%
         dplyr::filter(.data$symbol == gene) %>%
         dplyr::slice(1) %>%
         dplyr::pull(chr)
 
-    methy_data <- query_methy(
-        x,
+    .plot_region_heatmap(
+        x = x,
         chr = chr,
-        start = plot_left,
-        end = plot_right
+        start = pos_range[1],
+        end = pos_range[2],
+        window_prop = window_prop,
+        pos_style = pos_style,
+        subsample = subsample
     )
-
-    # add sample information
-    methy_data <- dplyr::left_join(
-        NanoMethViz::samples(x),
-        methy_data,
-        by = "sample",
-        multiple = "all"
-    )
-
-    read_data <- methy_data %>%
-        dplyr::group_by(.data$read_name) %>%
-        dplyr::summarise(start = min(.data$pos), end = max(.data$pos))
-
-    group_data <- methy_data %>%
-            dplyr::group_by(.data$read_name) %>%
-            dplyr::summarise(group = unique(.data$group))
-
-    # get read starts and ends
-    read_data <- dplyr::left_join(read_data, group_data, by = "read_name", multiple = "all")
-
-    # get grouping indices to pack reads
-    append_read_group <- function(x, k) {
-        x$read_group <- paste0(k, stacked_interval_inds(x))
-        x
-    }
-    grouping_data <- read_data %>%
-        dplyr::group_by(.data$group) %>%
-        dplyr::group_modify(append_read_group) %>%
-        dplyr::ungroup()
-
-
-    methy_data$mod_prob <- e1071::sigmoid(methy_data$statistic)
-
-    methy_data <- dplyr::left_join(
-        methy_data,
-        dplyr::select(grouping_data, "read_name", "read_group"),
-        by = "read_name",
-        multiple = "all"
-    )
-
-    # subsample reads down to a certain number of read groups
-    subsample_groups <- function(x, key, subsample) {
-        if (nrow(x) > subsample) {
-            x <- dplyr::sample_n(x, subsample)
-        }
-        x
-    }
-    methy_data <- methy_data %>%
-        dplyr::nest_by(.data$group, .data$read_group) %>%
-        dplyr::group_by(.data$group) %>%
-        dplyr::group_modify(subsample_groups, subsample = subsample)
-    methy_data <- tidyr::unnest(methy_data, "data")
-    read_data <- read_data %>%
-        dplyr::filter(.data$read_name %in% methy_data$read_name)
-
-    if (pos_style == "compact") {
-        # only plots sites with measured modification, evenly spaced
-        p <- ggplot(methy_data,
-            aes(x = factor(.data$pos),
-                y = .data$read_group,
-                fill = .data$mod_prob)) +
-            ggplot2::geom_raster() +
-            heatmap_fill_scale +
-            ggplot2::facet_wrap(~group, scales = "free_y", ncol = 1, strip.position = "right") +
-            theme_methy_heatmap +
-            ggplot2::theme(
-                axis.ticks.x = ggplot2::element_blank(),
-                axis.text.x = ggplot2::element_blank(),
-            ) +
-            ggplot2::xlab("Site")
-    } else if (pos_style == "to_scale") {
-        # plots all sites in range, evenly spaced with square geoms
-        # data will overlap
-        p <- ggplot(methy_data, aes(y = .data$read_group)) +
-            ggplot2::geom_errorbarh(
-                ggplot2::aes(
-                    xmin = .data$start,
-                    xmax = .data$end
-                ),
-                data = dplyr::left_join(
-                    read_data, grouping_data,
-                    by = c("read_name", "start", "end", "group"),
-                    multiple = "all"
-                ),
-                alpha = 1,
-                color = "darkgray",
-                linewidth = 1.2,
-                height = 0
-            ) +
-            ggplot2::geom_point(
-                aes(x = .data$pos, col = .data$mod_prob), alpha = 1, shape = 15) +
-            heatmap_col_scale +
-            ggplot2::facet_wrap(~group, scales = "free_y", ncol = 1, strip.position = "right") +
-            theme_methy_heatmap +
-            ggplot2::xlab("Position")
-    }
-
-    p
 }
--- R/plot_region_heatmap.R
+++ R/plot_region_heatmap.R
@@ -207,59 +207,11 @@ setMethod("plot_region_heatmap",
         multiple = "all"
     )
 
-    # subsample reads down to a certain number of read groups
-    subsample_groups <- function(x, key, subsample) {
-        if (nrow(x) > subsample) {
-            x <- dplyr::sample_n(x, subsample)
-        }
-        x
-    }
-    methy_data <- methy_data %>%
-        dplyr::nest_by(.data$group, .data$read_group) %>%
-        dplyr::group_by(.data$group) %>%
-        dplyr::group_modify(subsample_groups, subsample = subsample)
-    methy_data <- tidyr::unnest(methy_data, "data")
-    read_data <- read_data %>%
-        dplyr::filter(.data$read_name %in% methy_data$read_name)
-
-    if (pos_style == "compact") {
-        # only plots sites with measured modification, evenly spaced
-        p <- ggplot2::ggplot(methy_data, aes(x = factor(.data$pos), y = .data$read_group, fill = .data$mod_prob)) +
-            ggplot2::geom_raster() +
-            heatmap_fill_scale +
-            ggplot2::facet_wrap(~group, scales = "free_y", ncol = 1) +
-            theme_methy_heatmap +
-            ggplot2::theme(
-                axis.ticks.x = ggplot2::element_blank(),
-                axis.text.x = ggplot2::element_blank(),
-            ) +
-            ggplot2::xlab("Site")
-    } else if (pos_style == "to_scale") {
-        # plots all sites in range, evenly spaced with square geoms
-        # data will overlap
-        p <- ggplot2::ggplot(methy_data, aes(y = .data$read_group)) +
-            ggplot2::geom_errorbarh(
-                ggplot2::aes(
-                    xmin = .data$start,
-                    xmax = .data$end
-                ),
-                data = dplyr::left_join(
-                    read_data,
-                    grouping_data,
-                    by = c("read_name", "start", "end", "group"),
-                    multiple = "all"
-                ),
-                color = "darkgray",
-                linewidth = 1.2,
-                height = 0
-            ) +
-            ggplot2::geom_point(
-                aes(x = .data$pos, col = .data$mod_prob), alpha = 1, shape = 15) +
-            heatmap_col_scale +
-            ggplot2::facet_wrap(~group, scales = "free_y", ncol = 1, strip.position = "right") +
-            theme_methy_heatmap +
-            ggplot2::xlab("Position")
-    }
-
-    p
+    plot_heatmap_internal(
+        methy_data = methy_data,
+        read_data = read_data,
+        grouping_data = grouping_data,
+        pos_style = pos_style,
+        subsample = subsample
+    )
 }

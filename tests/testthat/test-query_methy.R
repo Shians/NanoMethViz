536a208ca44dfc4c633a2f7f7d0698ad03be87fd
Shians <registertonysu@gmail.com>
2023-05-22 17:28:32 +1000

    Added tests

--- tests/testthat/test-query_methy.R
+++ tests/testthat/test-query_methy.R
@@ -1,6 +1,16 @@
 test_that("Querying methylation works", {
     # setup
     nmr <- load_example_nanomethresult()
+    mbr <- ModBamResult(
+        methy = ModBamFiles(
+            paths = system.file(package = "NanoMethViz", "peg3.bam"),
+            samples = "sample1"
+        ),
+        samples = tibble::tibble(
+            sample = "sample1",
+            group = "group1"
+        )
+    )
 
     # test
     expect_silent(methy_data1 <- query_methy(methy(nmr), "chr7", 6703892, 6730431))
@@ -15,4 +25,21 @@ test_that("Querying methylation works", {
 
     expect_silent(gene_methy_data <- query_methy_gene(nmr, "Peg3"))
     expect_equal(methy_data1, gene_methy_data)
+
+    expect_error(query_methy_gene(nmr, "Missing"))
+    expect_error(query_methy(nmr, "Missing", 1, 1000))
+
+    regions <- data.frame(
+        chr = c("chr7", "chr7"),
+        start = c(6703892, 6717161),
+        end = c(6717162, 6730431)
+    )
+
+    expect_silent(query_methy_df(methy(nmr), regions))
+
+    regions_gr <- GenomicRanges::GRanges(regions)
+
+    query_methy_gr(methy(nmr), regions_gr)
+
+    expect_silent(query_methy(mbr, "chr7", 6703892, 6730431))
 })

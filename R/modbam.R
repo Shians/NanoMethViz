c25d9c4195756a6f7b99f6aa2fd4984a1eda6302
Shians <registertonysu@gmail.com>
2023-05-23 14:01:55 +1000

    Removed redundant code

--- R/modbam.R
+++ R/modbam.R
@@ -1,80 +1,22 @@
-rev_df <- function(x) {
-    assertthat::assert_that(is(x, "data.frame"))
-
-    x[rev(1:nrow(x)), ]
-}
-
-mod_tokeniser <- function(string, scores) {
-    string <- stringr::str_remove_all(string, ";")
-
-    str_split_comma <- function(x, ...) {
-        stringr::str_split(x, ",")[[1]]
-    }
-
-    mod_string_split <- str_split_comma(string)[-1] %>%
-        as.numeric()
-    mod_pos <- (mod_string_split + 1) %>%
-        cumsum()
-    mod_scores_split <- str_split_comma(scores) %>%
-        as.numeric()
-
-    data.frame(
-        mod_pos = mod_pos,
-        mod_prob = mod_scores_split/255
-    )
-}
-
-cigar_tokeniser <- function(x) {
-    non_empty_string <- function(x) { x != "" }
-    state <- stringr::str_split(x, "\\d+") %>%
-        unlist() %>%
-        purrr::keep(non_empty_string)
-    count <- stringr::str_split(x, "[[:alpha:]]") %>%
-        unlist() %>%
-        purrr::keep(non_empty_string)
-
-    data.frame(state = state, count = as.numeric(count))
-}
-
-get_coord_map <- function(cigar) {
-    tokens <- cigar_tokeniser_cpp(cigar)
-    token_vec <- rep(tokens$state, times = tokens$count)
-    seq_coord <- 0
-    ref_coord <- 0
-
-    seq_map <- numeric()
-    ref_map <- numeric()
-
-    for (tok in token_vec) {
-        if (tok == "M") {
-            seq_coord <- seq_coord + 1
-            ref_coord <- ref_coord + 1
-            seq_map <- c(seq_map, seq_coord)
-            ref_map <- c(ref_map, ref_coord)
-        } else if (tok %in% c("I", "S")) {
-            seq_coord <- seq_coord + 1
-            seq_map <- c(seq_map, seq_coord)
-            ref_map <- c(ref_map, NA)
-        } else if (tok %in% c("D", "N")) {
-            ref_coord <- ref_coord + 1
-        }
-    }
-
-    out <- setNames(ref_map, seq_map)
-    out
-}
-
+# get modification probability values and reference-based coordinates
 modbam_to_ref_coord <- function(seq, cigar, mod_str, mod_scores, map_pos, strand) {
+    # tokenise the cigar string into a data.frame
     mod_tokens <- mod_tokeniser_cpp(mod_str, mod_scores)
+
+    # get the mapping from strand coordinate to relative genomic coordinate
     coord_map <- get_coord_map_cpp(cigar)
 
+    # get positions of base of interest
     if (strand == "-") {
         mod_candidate_pos <- rev(get_char_pos_cpp(seq, "G"))[mod_tokens$mod_pos]
     } else if (strand == "+") {
         mod_candidate_pos <- get_char_pos_cpp(seq, "C")[mod_tokens$mod_pos]
     }
 
+    # calculate the relative position
     rel_pos <- coord_map[mod_candidate_pos]
+
+    # add the mapping position offset to the relative position
     if (strand == "-") {
         genome_pos <- map_pos + rel_pos - 2
     } else {
@@ -88,23 +30,14 @@ modbam_to_ref_coord <- function(seq, cigar, mod_str, mod_scores, map_pos, strand
         tidyr::drop_na()
 }
 
+
 read_modbam_table <- function(x, chr, start, end, sample) {
     bam_file <- Rsamtools::BamFile(x)
 
-    query <- GenomicRanges::GRanges(glue::glue("{chr}:{start}-{end}"))
-
-    empty_res <- tibble::tibble(
-        "sample" = character(),
-        "chr" = character(),
-        "pos" = integer(),
-        "strand" = character(),
-        "statistic" = numeric(),
-        "read_name" = character(),
-        "mod_prob" = numeric()
-    )
+    query <- make_granges(chr, start, end)
 
     if (length(query) == 0) {
-        return(list(empty_res))
+        return(list(empty_methy_query_output()))
     }
 
     sb_param <- Rsamtools::ScanBamParam(

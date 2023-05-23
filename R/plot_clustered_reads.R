6b6837f58ac7feda1026db3cb199fbcc63ef5fc2
Shians <registertonysu@gmail.com>
2023-05-23 17:28:58 +1000

    Refactored code

--- R/plot_clustered_reads.R
+++ R/plot_clustered_reads.R
@@ -1,7 +1,7 @@
 plot_clustered_reads <- function(x, chr, start, end) {
     methy_data <- query_methy(x, chr, start, end) %>%
-        filter(.data$pos > start & .data$pos < end) %>%
-        inner_join(samples(x), by = "sample")
+        dplyr::filter(.data$pos > start & .data$pos < end) %>%
+        dplyr::inner_join(samples(x), by = "sample")
 
     cluster_res <- cluster_reads(x, chr, start, end)
 
@@ -11,12 +11,12 @@ plot_clustered_reads <- function(x, chr, start, end) {
     }
 
     read_data <- methy_data %>%
-            dplyr::group_by(.data$read_name) %>%
-            dplyr::summarise(start = min(.data$pos), end = max(.data$pos))
+        dplyr::group_by(.data$read_name) %>%
+        dplyr::summarise(start = min(.data$pos), end = max(.data$pos))
 
     group_data <- methy_data %>%
-                dplyr::group_by(.data$read_name) %>%
-                dplyr::summarise(group = unique(.data$group))
+        dplyr::group_by(.data$read_name) %>%
+        dplyr::summarise(group = unique(.data$group))
 
     read_data <- dplyr::left_join(read_data, group_data, by = "read_name", multiple = "all")
 
@@ -25,7 +25,7 @@ plot_clustered_reads <- function(x, chr, start, end) {
         dplyr::group_modify(append_read_group) %>%
         dplyr::ungroup()
 
-    methy_data <- inner_join(methy_data, read_data)
+    methy_data <- dplyr::inner_join(methy_data, read_data)
 
     p1 <- plot_methylation_internal(
         methy_data,
@@ -42,13 +42,68 @@ plot_clustered_reads <- function(x, chr, start, end) {
         ggplot2::scale_x_continuous(labels = scales::label_number(scale_cut = scales::cut_si("b")))
 
     p2 <- plot_heatmap_internal(
-            methy_data = inner_join(methy_data, cluster_res),
-            pos_style = "to_scale",
-            subsample = 30,
-            group_col = "cluster_id"
-        ) +
+        methy_data = dplyr::inner_join(methy_data, cluster_res),
+        pos_style = "to_scale",
+        subsample = 30,
+        group_col = "cluster_id"
+    ) +
+        ggplot2::coord_cartesian(xlim = xlim, expand = FALSE) +
+        ggplot2::scale_x_continuous(labels = scales::label_number(scale_cut = scales::cut_si("b")))
+
+    p1 / p2
+}
+
+plot_clustered_reads2 <- function(x, chr, start, end) {
+    methy_data <- query_methy(x, chr, start, end) %>%
+        dplyr::filter(.data$pos > start & .data$pos < end) %>%
+        dplyr::inner_join(samples(x), by = "sample")
+
+    read_data <- methy_data %>%
+        dplyr::group_by(.data$read_name) %>%
+        dplyr::summarise(start = min(.data$pos), end = max(.data$pos))
+
+    group_data <- methy_data %>%
+        dplyr::group_by(.data$read_name) %>%
+        dplyr::summarise(group = unique(.data$group))
+
+    read_data <- dplyr::left_join(read_data, group_data, by = "read_name", multiple = "all")
+
+    append_read_group <- function(x, k) {
+        x$read_group <- paste0(k, stacked_interval_inds(x))
+        x
+    }
+
+    read_data <- read_data %>%
+        dplyr::group_by(.data$group) %>%
+        dplyr::group_modify(append_read_group) %>%
+        dplyr::ungroup()
+
+    methy_data <- dplyr::inner_join(methy_data, read_data)
+
+    cluster_res <- cluster_reads(x, chr, start, end)
+
+    p1 <- plot_methylation_internal(
+        methy_data,
+        sample_anno = samples(x),
+        read_anno = cluster_res,
+        chr = chr,
+        start = start,
+        end = end,
+        group_col = "cluster_id",
+        title = "title",
+        points = TRUE
+    ) +
         ggplot2::coord_cartesian(xlim = c(start, end), expand = FALSE) +
         ggplot2::scale_x_continuous(labels = scales::label_number(scale_cut = scales::cut_si("b")))
 
-    p1/p2
+    p2 <- plot_heatmap_internal(
+        methy_data = dplyr::inner_join(methy_data, cluster_res),
+        pos_style = "to_scale",
+        subsample = 30,
+        group_col = "cluster_id"
+    ) +
+        ggplot2::coord_cartesian(xlim = c(start, end), expand = FALSE) +
+        ggplot2::scale_x_continuous(labels = scales::label_number(scale_cut = scales::cut_si("b")))
+
+    p1 / p2
 }

90e84b82ef6220153491bdd5fa5d5f536a2e77bf
Shians <registertonysu@gmail.com>
2023-05-24 15:52:57 +1000

    Fixed tests

--- tests/testthat/test-plot_gene.R
+++ tests/testthat/test-plot_gene.R
@@ -1,25 +1,33 @@
 test_that("Plotting gene works", {
     # setup
     nmr <- load_example_nanomethresult()
-
-    # test
-    expect_silent(p <- plot_gene(nmr, "Peg3"))
-    expect_true(is(p, "ggplot"))
-
+    mbr <- load_example_modbamresult()
     params <- expand.grid(
         heatmap = c(TRUE, FALSE),
         spaghetti = c(TRUE, FALSE),
         gene_anno = c(TRUE, FALSE)
     )
 
-    for (i in 1:nrow(params)) {
-        expect_silent(
-            plot_gene(
-                nmr,
-                "Peg3",
-                heatmap = params$heatmap[i],
-                spaghetti = params$spaghetti[i]
+    # test
+    for (x in list(nmr, mbr)) {
+        expect_silent(p1 <- plot_gene(x, "Peg3"))
+        expect_s3_class(p1, "ggplot")
+
+        expect_silent(p2 <- plot_gene(x, "Peg3", heatmap = TRUE))
+        expect_s3_class(p2, "ggplot")
+
+        for (i in 1:nrow(params)) {
+            expect_silent(
+                p <- plot_gene(
+                    x, "Peg3",
+                    heatmap = params$heatmap[i],
+                    spaghetti = params$spaghetti[i]
+                )
             )
-        )
+
+            expect_s3_class(p, "ggplot")
+        }
+
+        expect_error(plot_gene(x, "missing_gene", heatmap = TRUE))
     }
 })
--- tests/testthat/test-plot_region.R
+++ tests/testthat/test-plot_region.R
@@ -2,30 +2,37 @@ test_that("Plotting region works", {
     # setup
     nmr <- load_example_nanomethresult()
     mbr <- load_example_modbamresult()
-
-    # test
-    expect_silent(p1 <- plot_region(nmr, "chr7", 6703892, 6730431))
-    expect_true(is(p1, "ggplot"))
-    expect_silent(p2 <- plot_region(mbr, "chr7", 6703892, 6730431))
-    expect_true(is(p2, "ggplot"))
-
-    expect_silent(plot_region(nmr, factor("chr7"), 6703892, 6730431))
-    expect_silent(plot_region(mbr, factor("chr7"), 6703892, 6730431))
-
     params <- expand.grid(
         heatmap = c(TRUE, FALSE),
         spaghetti = c(TRUE, FALSE),
         gene_anno = c(TRUE, FALSE)
     )
 
-    for (i in 1:nrow(params)) {
-        expect_silent(
-            plot_region(
-                nmr, "chr7", 6703892, 6730431,
-                heatmap = params$heatmap[i],
-                spaghetti = params$spaghetti[i]
+    # test
+    for (x in list(nmr, mbr)) {
+        expect_silent(p1 <- plot_region(x, "chr7", 6703892, 6730431))
+        expect_s3_class(p1, "ggplot")
+
+        expect_silent(p2 <- plot_region(x, "chr7", 6703892, 6730431, heatmap = TRUE))
+        expect_s3_class(p2, "ggplot")
+
+        expect_silent(p3 <- plot_region(x, factor("chr7"), 6703892, 6730431))
+        expect_s3_class(p3, "ggplot")
+
+        expect_silent(p4 <- plot_region(x, factor("chr7"), 6703892, 6730431, heatmap = TRUE))
+        expect_s3_class(p4, "ggplot")
+
+        for (i in 1:nrow(params)) {
+            expect_silent(
+                p <- plot_region(
+                    x, "chr7", 6703892, 6730431,
+                    heatmap = params$heatmap[i],
+                    spaghetti = params$spaghetti[i]
+                )
             )
-        )
+
+            expect_s3_class(p, "ggplot")
+        }
     }
 
     expect_warning(expect_error(plot_region(nmr, "unknown_chr", 6703892, 6730431)))

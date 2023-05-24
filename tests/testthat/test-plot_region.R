ff34dc67e3334f842548609ec6ee38575f2269b5
Shians <registertonysu@gmail.com>
2023-05-24 11:07:18 +1000

    Added tests

--- tests/testthat/test-plot_region.R
+++ tests/testthat/test-plot_region.R
@@ -1,10 +1,25 @@
 test_that("Plotting region works", {
     # setup
     nmr <- load_example_nanomethresult()
+    mbr <- ModBamResult(
+        methy = ModBamFiles(
+            paths = system.file(package = "NanoMethViz", "peg3.bam"),
+            samples = "sample1"
+        ),
+        samples = tibble::tibble(
+            sample = "sample1",
+            group = "group1"
+        )
+    )
 
     # test
-    expect_silent(p <- plot_region(nmr, "chr7", 6703892, 6730431))
-    expect_true(is(p, "ggplot"))
+    expect_silent(p1 <- plot_region(nmr, "chr7", 6703892, 6730431))
+    expect_true(is(p1, "ggplot"))
+    expect_silent(p2 <- plot_region(mbr, "chr7", 6703892, 6730431))
+    expect_true(is(p2, "ggplot"))
+
+    expect_silent(plot_region(nmr, factor("chr7"), 6703892, 6730431))
+    expect_silent(plot_region(mbr, factor("chr7"), 6703892, 6730431))
 
     params <- expand.grid(
         heatmap = c(TRUE, FALSE),
@@ -21,4 +36,7 @@ test_that("Plotting region works", {
             )
         )
     }
+
+    expect_warning(expect_error(plot_region(nmr, "unknown_chr", 6703892, 6730431)))
+    expect_error(plot_region(mbr, "unknown_chr", 6703892, 6730431))
 })

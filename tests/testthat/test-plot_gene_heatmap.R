9eb83bb12313171e185e2f4269a082c830f182ed
Shians <registertonysu@gmail.com>
2023-06-27 12:01:32 +1000

    Updated tests

--- tests/testthat/test-plot_gene_heatmap.R
+++ tests/testthat/test-plot_gene_heatmap.R
@@ -5,4 +5,13 @@ test_that("Plotting gene methylation heatmap works", {
     # test
     expect_silent(p1 <- plot_gene_heatmap(nmr, "Peg3"))
     expect_true(is(p1, "ggplot"))
+
+    # test for a bug whereby samples not present in data cause function to hang
+    nmr_extra_sample <- load_example_nanomethresult()
+    samples(nmr_extra_sample) <- bind_rows(
+        samples(nmr_extra_sample),
+        c(sample = "foo", group = "bar")
+    )
+    expect_silent(p <- plot_gene_heatmap(nmr_extra_sample, "Peg3"))
+    expect_true(is(p, "ggplot"))
 })

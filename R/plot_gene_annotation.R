c1b1c7b383fb3e2a6fd986bbfcc568c7cd4c48c7
Shians <registertonysu@gmail.com>
2023-08-18 16:48:42 +1000

    plot_region no longer reserves space for transcripts not visible in plotting area

--- R/plot_gene_annotation.R
+++ R/plot_gene_annotation.R
@@ -1,84 +1,5 @@
 plot_gene_annotation <- function(exons_df, plot_start, plot_end) {
-    if (nrow(exons_df) == 0) {
-        p <- ggplot() + theme_void()
-        attr(p, "plot_height") <- 0
-        return(p)
-    }
-
-    exons_df <- exons_df %>%
-        dplyr::mutate(
-            uid = factor(paste(.data$gene_id, .data$transcript_id, sep = ".")),
-            y_offset = as.integer(.data$uid) - 1
-        )
-
-    exons_count <- exons_df %>%
-        dplyr::group_by(.data$uid, .data$y_offset) %>%
-        dplyr::summarise(exons = dplyr::n())
-
-    gap <- exons_df %>%
-        dplyr::inner_join(exons_count, by = c("uid", "y_offset"), multiple = "all") %>%
-        dplyr::filter(.data$exons > 1) %>%
-        dplyr::group_by("transcript_id") %>%
-        dplyr::arrange(.data$start) %>%
-        dplyr::ungroup()
-
-    if (nrow(gap) > 0) {
-        gap <- gap %>%
-            dplyr::group_by(.data$uid, .data$y_offset, .data$strand) %>%
-            dplyr::summarise(
-                gap_start = list(.data$end[-length(.data$end)]),
-                gap_end = list(.data$start[-1])
-            ) %>%
-            tidyr::unnest(cols = c("gap_start", "gap_end"))
-    } else {
-        gap <- tibble::tibble(
-            uid = character(),
-            y_offset = numeric(),
-            strand = character(),
-            gap_start = numeric(),
-            gap_end = numeric()
-        )
-    }
-
-    .get_gaps <- function(gaps, strand = c("+", "-", "*")) {
-        strand <- match.arg(strand)
-
-        gaps <- gaps %>%
-            split(gaps$strand)
-
-        if (!is.null(gaps[[strand]])) {
-            out <- gaps[[strand]]
-
-            if (strand == "-") {
-                temp <- out$gap_start
-                out$gap_start <- out$gap_end
-                out$gap_end <- temp
-            }
-        } else {
-            out <- tibble::tibble(
-                uid = character(0),
-                y_offset = numeric(0),
-                strand = character(0),
-                gap_start = integer(0),
-                gap_end = integer(0)
-            )
-        }
-
-        dplyr::rename(
-            out,
-            start = "gap_start",
-            end = "gap_end"
-        )
-    }
-
-    gap_pos <- .get_gaps(gap, "+")
-    gap_neg <- .get_gaps(gap, "-")
-    gap_none <- .get_gaps(gap, "*")
-
-    gene_labels <- exons_df %>%
-        dplyr::group_by(.data$gene_id, .data$symbol, .data$transcript_id, .data$y_offset, .data$strand) %>%
-        dplyr::summarise(gene_middle = (min(.data$start) + max(.data$end)) / 2)
-
+    # helper functions ----
     .exons <- function(exons_df) {
         ggplot2::geom_rect(
             ggplot2::aes(
@@ -157,6 +78,25 @@ plot_gene_annotation <- function(exons_df, plot_start, plot_end) {
         )
     }
 
+    .filter_regions <- function(exons_df, plot_start, plot_end) {
+        transcripts <- exons_df %>%
+            dplyr::summarise(
+                .by = .data$transcript_id,
+                start = min(.data$start),
+                end = max(.data$end)
+            )
+
+        transcripts <- transcripts %>%
+            dplyr::filter(
+                .data$start <= plot_end & .data$end >= plot_start
+            )
+
+        exons_df %>%
+            dplyr::filter(
+                .data$transcript_id %in% transcripts$transcript_id
+            )
+    }
+
     .truncate_region <- function(x, plot_start, plot_end, strand) {
         if (strand == "-") {
             x <- x %>%
@@ -173,7 +113,92 @@ plot_gene_annotation <- function(exons_df, plot_start, plot_end) {
         x
     }
 
+    .get_gaps <- function(gaps, strand = c("+", "-", "*")) {
+        strand <- match.arg(strand)
+
+        gaps <- gaps %>%
+            split(gaps$strand)
+
+        if (!is.null(gaps[[strand]])) {
+            out <- gaps[[strand]]
+
+            if (strand == "-") {
+                temp <- out$gap_start
+                out$gap_start <- out$gap_end
+                out$gap_end <- temp
+            }
+        } else {
+            out <- tibble::tibble(
+                uid = character(0),
+                y_offset = numeric(0),
+                strand = character(0),
+                gap_start = integer(0),
+                gap_end = integer(0)
+            )
+        }
+
+        dplyr::rename(
+            out,
+            start = "gap_start",
+            end = "gap_end"
+        )
+    }
+
+    # function body ----
+    if (nrow(exons_df) == 0) {
+        p <- ggplot() + theme_void()
+        attr(p, "plot_height") <- 0
+        return(p)
+    }
+
+    # remove transcripts outside plot area
+    exons_df <- .filter_regions(exons_df, plot_start, plot_end)
+
+    exons_df <- exons_df %>%
+        dplyr::mutate(
+            uid = factor(paste(.data$gene_id, .data$transcript_id, sep = ".")),
+            y_offset = as.integer(.data$uid) - 1
+        )
+
+    exons_count <- exons_df %>%
+        dplyr::group_by(.data$uid) %>%
+        dplyr::summarise(exons = dplyr::n())
+
+    gap <- exons_df %>%
+        dplyr::inner_join(exons_count, by = c("uid"), multiple = "all") %>%
+        dplyr::filter(.data$exons > 1) %>%
+        dplyr::group_by("transcript_id") %>%
+        dplyr::arrange(.data$start) %>%
+        dplyr::ungroup()
+
+    if (nrow(gap) > 0) {
+        gap <- gap %>%
+            dplyr::group_by(.data$uid, .data$strand, .data$y_offset) %>%
+            dplyr::summarise(
+                gap_start = list(.data$end[-length(.data$end)]),
+                gap_end = list(.data$start[-1])
+            ) %>%
+            tidyr::unnest(cols = c("gap_start", "gap_end"))
+    } else {
+        gap <- tibble::tibble(
+            uid = character(),
+            y_offset = numeric(),
+            strand = character(),
+            gap_start = numeric(),
+            gap_end = numeric()
+        )
+    }
+
+    gap_pos <- .get_gaps(gap, "+")
+    gap_neg <- .get_gaps(gap, "-")
+    gap_none <- .get_gaps(gap, "*")
+
+    gene_labels <- exons_df %>%
+        dplyr::group_by(.data$gene_id, .data$symbol, .data$transcript_id, .data$y_offset, .data$strand) %>%
+        dplyr::summarise(gene_middle = (min(.data$start) + max(.data$end)) / 2)
+
     exons_df <- .truncate_region(exons_df, plot_start, plot_end, "*")
+
     gap_pos <- .truncate_region(gap_pos, plot_start, plot_end, "+")
     gap_neg <- .truncate_region(gap_neg, plot_start, plot_end, "-")
     gap_none <- .truncate_region(gap_none, plot_start, plot_end, "*")

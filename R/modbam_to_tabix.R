852fb823d22c360b0031dd7ab4def58d94493219
shians <registertonysu@gmail.com>
2024-02-16 17:14:01 +1100

    Refactored modbam to tabix conversion

--- R/modbam_to_tabix.R
+++ R/modbam_to_tabix.R
@@ -35,6 +35,26 @@
 modbam_to_tabix <- function(x, out_file, mod_code = NanoMethViz::mod_code(x)) {
     assertthat::assert_that(is(x, "ModBamResult"))
 
+    if (fs::file_exists(out_file)) {
+        cli::cli_progress_step(paste0("Output file exists, overwriting ", out_file))
+        fs::file_delete(out_file)
+    }
+
+    cli::cli_progress_step("Converting data to TSV")
+    tsv_file <- convert_modbam_to_tsv(x, out_file, mod_code)
+
+    cli::cli_progress_step("Sorting data")
+    f <- sort_methy_file(tsv_file)
+
+    cli::cli_progress_step("Compressing data")
+    out <- tabix_compress(f)
+    fs::file_delete(f)
+
+    cli::cli_progress_step(paste0("Tabix file created: ", out))
+    invisible(out)
+}
+
+convert_modbam_to_tsv <- function(x, out_file, mod_code) {
     # if .gz at end of output name then trim it so final output
     # doesn't end with .bgz.bgz
     if (stringr::str_detect(out_file, ".bgz$")) {
@@ -43,11 +63,6 @@ modbam_to_tabix <- function(x, out_file, mod_code = NanoMethViz::mod_code(x)) {
     }
 
     bam_info <- dplyr::inner_join(samples(x), methy(x), by = dplyr::join_by(sample))
-
-    if (fs::file_exists(out_file)) {
-        cli::cli_progress_step(paste0("Output file exists, overwriting ", out_file))
-        fs::file_delete(out_file)
-    }
 
     parse_read_chunk <- function(x) {
         parse_modbam(x[[1]], sample, mod_code = mod_code) %>%
@@ -58,41 +73,36 @@ modbam_to_tabix <- function(x, out_file, mod_code = NanoMethViz::mod_code(x)) {
     for (i in seq_len(n_files)) {
         path <- bam_info$path[i]
         sample <- bam_info$sample[i]
-        bam_file <- Rsamtools::BamFile(path, yieldSize = 15000)
 
         total <- get_bam_total_reads(path)
         fname <- fs::path_file(path)
-        cli::cli_progress_bar(
+
+        prog_bar_id <- cli::cli_progress_bar(
             glue::glue("Converting file {i}/{n_files}: {fname}"),
             total = total,
-            format_done = paste0(
-                "{.alert-success Data converted: ", fname, " {.timestamp {cli::pb_elapsed}}}"),
-            format_failed = paste0(
-                "{.alert-danger Data conversion failed: ", fname, " {.timestamp {cli::pb_elapsed}}}"),
+            format_done = paste0("{.alert-success Data converted: ", fname, " {.timestamp {cli::pb_elapsed}}}"),
+            format_failed = paste0("{.alert-danger Data conversion failed: ", fname, " {.timestamp {cli::pb_elapsed}}}"),
             clear = FALSE
         )
 
+        bam_file <- Rsamtools::BamFile(path, yieldSize = 15000)
         open(bam_file)
         while (Rsamtools::isIncomplete(bam_file)) {
             reads <- read_bam(bam_file)
-            cli::cli_progress_update(length(reads[[1]][[1]]))
+
+            n_reads <- length(reads[[1]][[1]])
+            cli::cli_progress_update(n_reads, id = prog_bar_id)
+
             if (!is.null(reads[[1]]) && length(reads[[1]]$qname) > 0) {
                 # parse if valid data exists
                 data <- parse_read_chunk(reads)
                 readr::write_tsv(data, out_file, append = TRUE, progress = FALSE)
             }
         }
-        close(bam_file)
+
     }
 
-    cli::cli_progress_step("Sorting data")
+    close(bam_file)
 
-    f <- sort_methy_file(out_file)
-
-    cli::cli_progress_step("Compressing data")
-    out <- tabix_compress(f)
-    fs::file_delete(f)
-
-    cli::cli_progress_step(paste0("Tabix file created: ", out))
-    invisible(out)
+    out_file
 }

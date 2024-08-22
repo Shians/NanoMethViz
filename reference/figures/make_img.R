124f6ad724965158d3f053336f2836b97e75f4da
Shians <Shians@users.noreply.github.com>
2024-08-22 04:07:35 +0000

    Deploying to gh-pages from @ Shians/NanoMethViz@b5cc72cda76095a05b90e329cb7c589fe4f65221 🚀

---
+++ reference/figures/make_img.R
@@ -0,0 +1,39 @@
+library(NanoMethViz)
+library(bsseq)
+library(stringr)
+
+nmr <- load_example_nanomethresult()
+bss <- nmr %>% methy_to_bsseq()
+samples <- colData(bss)$sample %>%
+    str_replace(".*_(\\d)_(bl6|cast)$", "\\2 \\1")
+groups <- str_extract(samples, "(bl6|cast)")
+
+ggsave(
+    "img/mds.png",
+    plot_mds(
+        bsseq_to_log_methy_ratio(
+            bss,
+            regions = exons_to_genes(NanoMethViz::exons(nmr))
+        ),
+        labels = samples,
+        groups = groups
+    ) +
+        ggtitle("MDS plot"),
+    height = 600, width = 800, units = "px", dpi = 150
+)
+
+
+ggsave(
+    "img/peg3_gene.png",
+    plot_gene(nmr, "Peg3"),
+    height = 600, width = 800, units = "px", dpi = 150)
+
+ggsave(
+    "img/agg_genes.png",
+    plot_agg_regions(
+        nmr,
+        regions = exons_to_genes(NanoMethViz::exons(nmr))
+    ) +
+        ggtitle("Aggregated profile over genes"),
+    height = 600, width = 800, units = "px", dpi = 150)
+

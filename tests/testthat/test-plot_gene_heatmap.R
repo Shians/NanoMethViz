2a590d72cef5d76ebae95e904f6687a21f838e70
shians <registertonysu@gmail.com>
2025-08-08 20:43:53 +1000

    Updated tests

--- tests/testthat/test-plot_gene_heatmap.R
+++ tests/testthat/test-plot_gene_heatmap.R
@@ -15,3 +15,50 @@ test_that("Plotting gene methylation heatmap works", {
     p <- expect_no_warning(plot_gene_heatmap(nmr_extra_sample, "Peg3"))
     expect_s3_class(p, "ggplot")
 })
+
+test_that("plot_gene_heatmap works with different parameters", {
+    nmr <- load_example_nanomethresult()
+
+    # test with different window_prop values
+    p <- expect_no_warning(plot_gene_heatmap(nmr, "Peg3", window_prop = 0.5))
+    expect_s3_class(p, "ggplot")
+
+    # test with vector window_prop
+    p <- expect_no_warning(plot_gene_heatmap(nmr, "Peg3", window_prop = c(0.2, 0.4)))
+    expect_s3_class(p, "ggplot")
+
+    # test with compact pos_style
+    p <- expect_no_warning(plot_gene_heatmap(nmr, "Peg3", pos_style = "compact"))
+    expect_s3_class(p, "ggplot")
+
+    # test with different subsample value
+    p <- expect_no_warning(plot_gene_heatmap(nmr, "Peg3", subsample = 25))
+    expect_s3_class(p, "ggplot")
+})
+
+test_that("plot_gene_heatmap error handling", {
+    nmr <- load_example_nanomethresult()
+
+    # test with empty exons
+    nmr_no_exons <- nmr
+    nmr_no_exons@exons <- tibble::tibble(
+        gene_id = character(0),
+        chr = character(0),
+        strand = character(0),
+        start = integer(0),
+        end = integer(0),
+        transcript_id = character(0),
+        symbol = character(0)
+    )
+
+    expect_error(
+        plot_gene_heatmap(nmr_no_exons, "Peg3"),
+        "exons\\(x\\) is empty, gene cannot be queried"
+    )
+
+    # test with gene not in annotation
+    expect_error(
+        plot_gene_heatmap(nmr, "NonExistentGene"),
+        "gene NonExistentGene not found in exon annotation"
+    )
+})
--- tests/testthat/test-plot_region_heatmap.R
+++ tests/testthat/test-plot_region_heatmap.R
@@ -10,7 +10,28 @@ test_that("Plotting region methylation heatmap works", {
     expect_s3_class(p, "ggplot")
 })
 
-test_that("Plotting works with GRanges",  {
+test_that("plot_region_heatmap works with different parameters", {
+    nmr <- load_example_nanomethresult()
+
+    # test with different window_prop values
+    expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6703892, 6730431, window_prop = 0.1))
+    expect_s3_class(p, "ggplot")
+
+    # test with vector window_prop
+    expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6703892, 6730431, window_prop = c(0.05, 0.15)))
+    expect_s3_class(p, "ggplot")
+
+    # test with different subsample value
+    expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6703892, 6730431, subsample = 30))
+    expect_s3_class(p, "ggplot")
+
+    # test with factor chromosome
+    chr_factor <- as.factor("chr7")
+    expect_no_warning(p <- plot_region_heatmap(nmr, chr_factor, 6703892, 6730431, pos_style = "to_scale"))
+    expect_s3_class(p, "ggplot")
+})
+
+test_that("Plotting works with GRanges", {
     nmr <- load_example_nanomethresult()
     gr <- GenomicRanges::GRanges(
         seqnames = "chr7",
@@ -28,7 +49,7 @@ test_that("Plotting works with GRanges",  {
 test_that("Plotting region methylation works without exons", {
     # setup
     nmr <- load_example_nanomethresult()
-    nmr@exons <- tibble(
+    nmr@exons <- tibble::tibble(
         gene_id = character(0),
         chr = character(0),
         strand = character(0),
@@ -38,4 +59,17 @@ test_that("Plotting region methylation works without exons", {
         symbol = character(0))
 
     expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6703892, 6730431))
+})
+
+test_that("plot_region_heatmap window calculation works correctly", {
+    nmr <- load_example_nanomethresult()
+
+    # test that window calculations work correctly by testing edge regions
+    # small region to test window calculation
+    expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6720000, 6720100, window_prop = 0.5))
+    expect_s3_class(p, "ggplot")
+
+    # test zero window
+    expect_no_warning(p <- plot_region_heatmap(nmr, "chr7", 6720000, 6720100, window_prop = 0))
+    expect_s3_class(p, "ggplot")
 })
